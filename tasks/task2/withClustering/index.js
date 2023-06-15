8f41b185336bc6915bca52fa07b15db043d658f8
avlian-90 <lianavagyan90@gmail.com>
2023-06-15 20:04:12 +0400

    fix: promisificated and refactored

--- tasks/task2/withClustering/index.js
+++ tasks/task2/withClustering/index.js
@@ -5,54 +5,65 @@ import os from "os";
 
 function converter(directoryPath) {
 
+  return new Promise((resolve, reject) => {
+
     fs.readdir(directoryPath, (err, files) => {
-        if (err) {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+  
+      const csvFiles = files.filter(file => path.extname(file).toLowerCase() === ".csv");
+  
+      if (csvFiles.length === 0) {
+        console.error("No CSV files!");
+        reject(new Error("No CSV files!"));
+        return;
+      }
+  
+      const filesCount = csvFiles.length;
+      let startTime = new Date();
+  
+  
+      const workersCount = Math.min(filesCount, os.cpus().length);
+      const recordsPerWorker = Math.ceil(filesCount / workersCount);
+  
+      let completedWorkers = 0;
+      let overallDuration = 0;
+      let ovearallRecordCount = 0;
+  
+      for (let i = 0; i < workersCount; i++) {
+
+        const start = i * recordsPerWorker;
+        const end = start + recordsPerWorker;
+        const worker = fork("tasks/task2/withClustering/worker.js");
+  
+        worker.send({ directoryPath, csvFiles: csvFiles.slice(start, end) });
+  
+        worker.on("message", (message) => {
+
+          const endTime = new Date();
+          const duration = endTime - startTime;
+          overallDuration += duration;
+          ovearallRecordCount += message.recordCount;
+           
+
+          completedWorkers ++;
+
+          if (completedWorkers === workersCount) {
+              console.log(`Total record count: ${ovearallRecordCount}. Duration: ${overallDuration}ms`); 
+              resolve();
+          }
+        });
+  
+        worker.on("error", (err) => {
           console.error(err);
-          process.exit();
-        }
-    
-        const csvFiles = files.filter(file => path.extname(file).toLowerCase() === ".csv");
-    
-        if (csvFiles.length === 0) {
-          console.error("No CSV files!");
-          process.exit();
-        }
-    
-        const filesCount = csvFiles.length;
-        let startTime = new Date();
-    
-    
-        const workersCount = Math.min(filesCount, os.cpus().length);
-        const recordsPerWorker = Math.ceil(filesCount / workersCount);
-    
-        let completedWorkers = 0;
-    
-        for (let i = 0; i < workersCount; i++) {
-
-          const start = i * recordsPerWorker;
-          const end = start + recordsPerWorker;
-          const worker = fork("tasks/task2/withClustering/worker.js");
-    
-          worker.send({ directoryPath, csvFiles: csvFiles.slice(start, end) });
-    
-          worker.on("message", (message) => {
-
-            const endTime = new Date();
-            const duration = endTime - startTime;
-            console.log(`Total record count: ${message.recordCount}. Duration: ${duration}ms`);  
-
-            completedWorkers ++;
-
-            if (completedWorkers === workersCount) {
-                process.exit();
-            }
-          });
-    
-          worker.on("error", (err) => {
-            console.error(err);
-          });
-        }
-    });
+          reject(err);
+        });
+      }
+  });
+  })
 }
 
 const directoryPath = process.argv[2];
@@ -62,5 +73,13 @@ if (!directoryPath) {
     process.exit();
 }
 
-converter(directoryPath);
+converter(directoryPath)
+  .then(() => {
+    console.log("Success!");
+    process.exit();
+  })
+  .catch((err) => {
+    console.err("Failed!", err);
+    process.exit();
+  })
 
